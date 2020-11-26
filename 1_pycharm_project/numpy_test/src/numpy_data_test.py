708ba71bd814abc027df03e4dd88698b4ae2a9e9
XJY <18822148155@163.com>
2020-11-26 15:09:28 +0800

    plt_show_scatter

---
+++ 1_pycharm_project/numpy_test/src/numpy_data_test.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# Author : calmcar_XJY
+# Date : 2020/11/25 下午6:06
+# Function:
+
+'''
+
+'''
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+
+
+def parse_npy_data(data_name):
+    print("*"*100)
+
+    input_npy_data = np.load(data_name)
+    one_ndim_npy_data = input_npy_data.flatten()
+    size_len = input_npy_data.size
+    print("one_ndim_npy_data.szie : {0}, \nnp.max(one_ndim_npy_data) : {1}, \nnp.min(one_ndim_npy_data) : {2}"
+                                                .format(one_ndim_npy_data.size,
+                                                np.max(one_ndim_npy_data),
+                                                np.min(one_ndim_npy_data), ))
+
+    print("*"*100)
+
+    for i in range(0, size_len):
+        plt.suptitle(data_name)
+        if data_name == data_name_float:
+
+            plt.scatter(i, one_ndim_npy_data[i], c= 'b')
+        else:
+            plt.scatter(i, one_ndim_npy_data[i], c= "r")
+
+
+    new_pic_name = "../result_img/{}.png".format(data_name.split("/")[-1])
+    plt.savefig(new_pic_name)
+    plt.show()
+
+def merge_float_uint8(data_name_float, data_name_uint8):
+    input_npy_data_float = np.load(data_name_float)
+    input_npy_data_uint8 = np.load(data_name_uint8)
+
+    X = np.arange(0, input_npy_data_float.size)
+
+    plt.subplot(2,1,1)
+    plt.scatter(X, input_npy_data_float * 128,s=5, c= "b", marker="x", label = "float")
+    plt.scatter(X, input_npy_data_uint8,s=5, c= "r", label = "uint8")
+    plt.legend()
+    plt.xlabel("data_size")
+    plt.ylabel("fitting")
+
+    plt.subplot(2, 1, 2)
+    plt.scatter(X, input_npy_data_float, s=5, c="b", marker = "x", label = "float")
+    plt.scatter(X, input_npy_data_uint8 / 128, s=5, c="r", label = "uint8")
+    plt.legend()
+    plt.xlabel("data_size")
+    plt.ylabel("fitting")
+
+
+    plt.savefig("../result_img/{}_{}_merge.png".format(data_name_float.split("/")[-1].split(".")[0]
+                                                    ,data_name_uint8.split("/")[-1].split(".")[0]),
+                figsize = (1920, 1080),
+                dpi = 1080)
+    plt.show()
+
+
+def main():
+    print("ok")
+    parse_npy_data(data_name_float)
+    parse_npy_data(data_name_uint8)
+    merge_float_uint8(data_name_float, data_name_uint8)
+
+
+if __name__ == "__main__":
+
+
+    data_name_float = "../data/float.npy"
+    data_name_uint8 = "../data/uint8.npy"
+
+    main()
---
+++ 1_pycharm_project/numpy_test/test/init.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Author : calmcar_XJY
+# Date : 2020/11/26 下午2:22
+# Function:
+
+'''
+
+'''
+
+import cv2
+import numpy as np
+
+im = cv2.imread("./000000512537.jpg")
+central_crop = 0.875
+resize_strategy = "tf"
+
+cv2.imshow("im", im)
+print("im.shape； ", im.shape)
+
+try:
+    im_dim = im.shape[2]
+except IndexError:
+    im_dim = 1
+
+if im_dim == 3:
+    rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+elif im_dim == 4:
+    rgb = cv2.cvtColor(im, cv2.COLOR_BGRA2RGB)
+else:
+    rgb = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
+
+
+#1 .cnetral crop
+h, w = im.shape[0], im.shape[1]
+
+h_cropped = int(h * central_crop)
+w_cropped = int(w * central_crop)
+
+h_start = (h-h_cropped) // 2
+h_end = h_cropped + h_start
+
+w_start = (w-w_cropped) // 2
+w_end = w_cropped + w_start
+
+image = rgb[h_start : h_end, w_start : w_end]
+
+cv2.imshow("image", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+print("image.shape : ", image.shape)
+
+#2. resize to target shape
+(scaled_w, scaled_h) = (224, 224)
+if resize_strategy == "tf":
+    import tensorflow as tf
+    image = np.expand_dims(image, 0)
+    '''
+    原始image         : 0 1 2    : h w c          : image.shape :  (437, 291, 3)
+    np添加维度后 image : 0 1 2 3  : 新添维度 h w c  : np add dims :  (1, 437, 291, 3)
+    '''
+    print("np add dims : ", image.shape)
+
+    tf_resized_image = tf.compat
+
+
+
+
+
+
+
+
+cv2.waitKey()
+cv2.destroyAllWindows()
